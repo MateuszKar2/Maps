ac096d781d77550efef7391586572d9d70cbd4e9
MateuszKar2 <mateuszwasabi@gmial.com>
2024-10-21 20:45:42 +0200

    Szkielet mojego projektu

---
+++ src/Company.ts
@@ -0,0 +1,20 @@
+import faker from 'faker';
+
+
+export class Company {
+
+  conpanyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  }
+  constructor() {
+    this.conpanyName = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+};
